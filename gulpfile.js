926576e0b4be5d3c4a614cb1258f314ad95652f7
Nenad <nenadmilunov91@gmail.com>
2017-12-15 20:25:07 +0100

    BEM part of the course

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function() {
     console.log("yey");
@@ -15,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() { //pazi za style i gresku
     return gulp.src('./app/assets/styles/style.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
